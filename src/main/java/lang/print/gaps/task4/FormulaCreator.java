c85e253d222bdfd280bc64a464ecf32a29d3b5e8
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-17 15:18:37 +0500

    s

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,13 +3,9 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
     int a = 5,b =5;
-        double v = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-        double t = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2;
-        double c =(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-        double c1 =(9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-        double c11 =(9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-        double c2 =(9*Math.pow(a, 2) - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
-        double c22 =(9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2;
+        double v = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
+        double t = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        double c =(9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2;
 
         }
 
