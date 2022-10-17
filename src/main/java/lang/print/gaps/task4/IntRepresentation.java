cd4683238c394fac61db9189bd97e8a98b4cfe6f
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-17 15:00:52 +0500

    asd

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c =99;
+        char c1 =104;
+        char c2 =97;
+        char c3 =114;
+        System.out.println(c);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
     }
 }
