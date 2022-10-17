d8bf536bf7e13111a7f47bcd5cec0c70f1ff0ae4
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-17 15:06:01 +0500

    s

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,5 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
     int a = 5,b =5;
         double v = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
+        double t = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        double c =(9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2;
     }
 }
